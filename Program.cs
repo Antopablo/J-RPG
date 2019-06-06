7aa0053998e7db160c61befa82a7123834d08a77
Antony LEFEVRE <lefevreanto57@gmail.com>
2019-06-06 15:25:04 +0200

    merge frank+moi ok

--- Program.cs
+++ Program.cs
@@ -11,9 +11,19 @@ namespace J_RPG
 {
     class Program
     {
+        static public void InitialiserTOUSpersonnage(List<Personnage> List_Perso_Recrutable, FabriquePersonnage fabrique)
+        {
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("ARCHER", "Archer"));
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("ASSASSIN", "Assassin"));
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("CHAMAN", "Chaman"));
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("CHEVALIER", "Chevalier"));
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("GUERRIER", "Guerrier"));
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("MAGE", "Mage"));
+            List_Perso_Recrutable.Add(fabrique.CreerPerso("PRETRE", "Prêtre"));
+        }
+
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             FabriquePersonnage fabriquePerso = new FabriquePersonnage();
             List<Personnage> List_Perso_Recrutable = new List<Personnage>(); // TOUS les personnages
             List<Personnage> List_Team = new List<Personnage>(); // Notre équipe de 3 perso max
@@ -37,20 +47,7 @@ namespace J_RPG
             Liste_TEAM.Add(perso);
             perso.DessinerPersonnage(UserChoice_Class.ToUpper());
             Console.WriteLine(perso);
-        }
-        static public void InitialiserTOUSpersonnage(List<Personnage> List_Perso_Recrutable, FabriquePersonnage fabrique)
-        {
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("ARCHER", "Archer"));
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("ASSASSIN", "Assassin"));
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("CHAMAN", "Chaman"));
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("CHEVALIER", "Chevalier"));
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("GUERRIER", "Guerrier"));
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("MAGE", "Mage"));
-            List_Perso_Recrutable.Add(fabrique.CreerPerso("PRETRE", "Prêtre"));
-        }
-    }
-}
-=======
+
             int ratio = 80;
             ConsoleWindowConfiguration.Setup();
             ConsoleConfiguration.Setup();
@@ -62,17 +59,9 @@ namespace J_RPG
             //Console.WriteLine("LargestWindowHeight : {0}", Console.LargestWindowHeight);
             int consoleWidth = ((Console.LargestWindowWidth * ratio / 100) % 2 == 0) ? Console.LargestWindowWidth * ratio / 100 - 1 : Console.LargestWindowWidth * ratio / 100;
             int consoleHeight = ((Console.LargestWindowHeight * ratio / 100) % 2 == 0) ? Console.LargestWindowHeight * ratio / 100 - 1 : Console.LargestWindowHeight * ratio / 100;
-            //Console.WriteLine("consoleWidth : {0}", consoleWidth);
-            //Console.WriteLine("consoleHeight : {0}", consoleHeight);
-            //Console.BufferWidth = consoleWidth;
-            //Console.BufferHeight = consoleHeight;
-            //Console.WindowTop = 0;
-            //Console.WindowLeft = 0;
-            //Console.WindowWidth = consoleWidth;
-            //Console.WindowHeight = consoleHeight;
 
 
-            if (false)
+            if (true)
             {
                 Console.SetBufferSize(500 + consoleWidth, 180 + consoleHeight);
                 Console.SetWindowSize(consoleWidth, consoleHeight);
@@ -97,69 +86,6 @@ namespace J_RPG
                 dataContractJsonSerializer = new DataContractJsonSerializer(typeof(char[][]));
                 worldMap = (char[][])dataContractJsonSerializer.ReadObject(stream);
                 stream.Close();
-
-
-                //for (int j = 0; j < worldMap[0].Length; j++)
-                //{
-                //    for (int i = 0; i < worldMap.Length; i++)
-                //    {
-                //        if (!usedChar.Contains(worldMap[i][j]))
-                //        {
-                //            worldMap[i][j] = ' ';
-                //        }
-                //    }
-                //}
-
-                //for (int j = 99; j > 80; j--)
-                //{
-                //    for (int i = 232; i < 269; i++)
-                //    {
-                //        test[i][j] = test[i][j - 1];
-                //        test[i][j - 1] = test[231][100];
-                //    }
-                //}
-
-                //for (int j = 0; j < test[0].Length; j++)
-                //{
-                //    for (int i = 0; i < test.Length; i++)
-                //    {
-                //        if (test[i][j] == '-')
-                //        {
-                //            test[i][j] = test[7][3];
-                //        }
-                //        if (test[i][j] == ':')
-                //        {
-                //            test[i][j] = test[6][4];
-                //        }
-                //        if (test[i][j] == '$')
-                //        {
-                //            test[i][j] = test[43][1];
-                //        }
-                //    }
-                //}
-
-                //string line = "";
-                //int compteur = 0;
-                //StreamReader rStream = new StreamReader(path + "map.txt");
-                //while (line != null)
-                //{
-                //    line = rStream.ReadLine();
-                //    if (line != null)
-                //    {
-                //        for (int i = 0; i < line.Length; i++)
-                //        {
-                //            test[i][compteur] = line[i];
-                //        }
-                //        compteur++;
-                //    }
-                //}
-
-                //path = @"..\..\Maps\";
-                //stream = new StreamWriter(path + "WorldMap.json").BaseStream;
-                //dataContractJsonSerializer = new DataContractJsonSerializer(typeof(char[][]));
-                //dataContractJsonSerializer.WriteObject(stream, test);
-                //stream.Close();
-
                 StringBuilder mapStringBuilder = new StringBuilder();
                 for (int j = 0; j < Console.BufferHeight; j++)
                 {
@@ -181,8 +107,6 @@ namespace J_RPG
                 Console.Write(mapStringBuilder.ToString());
 
 
-                //Console.SetWindowPosition((Console.BufferWidth - Console.WindowWidth) / 2, (Console.BufferHeight - Console.WindowHeight) / 2 + 17);
-                //Console.SetCursorPosition(Console.WindowLeft + Console.WindowWidth / 2, Console.WindowTop + Console.WindowHeight / 2);
                 Console.SetCursorPosition(Console.BufferWidth / 2, Console.BufferHeight / 2 + 17);
                 Console.SetWindowPosition(Console.CursorLeft - Console.WindowWidth / 2, Console.CursorTop - Console.WindowHeight / 2);
                 Console.Write("@");
@@ -323,135 +247,6 @@ namespace J_RPG
         }
     }
 }
-/*
-// This example demonstrates the Console.WindowLeft and
-//                               Console.WindowTop properties.
-using System;
-using System.Text;
-using System.IO;
-//
-class Sample
-{
-    public static int saveBufferWidth;
-    public static int saveBufferHeight;
-    public static int saveWindowHeight;
-    public static int saveWindowWidth;
-    public static bool saveCursorVisible;
-    //
-    public static void Main()
-    {
-        string m1 = "1) Press the cursor keys to move the console window.\n" +
-                    "2) Press any key to begin. When you're finished...\n" +
-                    "3) Press the Escape key to quit.";
-        string g1 = "+----";
-        string g2 = "|    ";
-        string grid1;
-        string grid2;
-        StringBuilder sbG1 = new StringBuilder();
-        StringBuilder sbG2 = new StringBuilder();
-        ConsoleKeyInfo cki;
-        int y;
-        //
-        try
-        {
-            saveBufferWidth = Console.BufferWidth;
-            saveBufferHeight = Console.BufferHeight;
-            saveWindowHeight = Console.WindowHeight;
-            saveWindowWidth = Console.WindowWidth;
-            saveCursorVisible = Console.CursorVisible;
-            //
-            Console.Clear();
-            Console.WriteLine(m1);
-            Console.ReadKey(true);
-
-            // Set the smallest possible window size before setting the buffer size.
-            Console.SetWindowSize(1, 1);
-            Console.SetBufferSize(80, 80);
-            Console.SetWindowSize(40, 20);
-
-            // Create grid lines to fit the buffer. (The buffer width is 80, but
-            // this same technique could be used with an arbitrary buffer width.)
-            for (y = 0; y < Console.BufferWidth / g1.Length; y++)
-            {
-                sbG1.Append(g1);
-                sbG2.Append(g2);
-            }
-            sbG1.Append(g1, 0, Console.BufferWidth % g1.Length);
-            sbG2.Append(g2, 0, Console.BufferWidth % g2.Length);
-            grid1 = sbG1.ToString();
-            grid2 = sbG2.ToString();
-
-            Console.CursorVisible = false;
-            Console.Clear();
-            for (y = 0; y < Console.BufferHeight - 1; y++)
-            {
-                if (y % 3 == 0)
-                    Console.Write(grid1);
-                else
-                    Console.Write(grid2);
-            }
-
-            Console.SetWindowPosition(0, 0);
-            do
-            {
-                cki = Console.ReadKey(true);
-                switch (cki.Key)
-                {
-                    case ConsoleKey.LeftArrow:
-                        if (Console.WindowLeft > 0)
-                            Console.SetWindowPosition(
-                                    Console.WindowLeft - 1, Console.WindowTop);
-                        break;
-                    case ConsoleKey.UpArrow:
-                        if (Console.WindowTop > 0)
-                            Console.SetWindowPosition(
-                                    Console.WindowLeft, Console.WindowTop - 1);
-                        break;
-                    case ConsoleKey.RightArrow:
-                        if (Console.WindowLeft < (Console.BufferWidth - Console.WindowWidth))
-                            Console.SetWindowPosition(
-                                    Console.WindowLeft + 1, Console.WindowTop);
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (Console.WindowTop < (Console.BufferHeight - Console.WindowHeight))
-                            Console.SetWindowPosition(
-                                    Console.WindowLeft, Console.WindowTop + 1);
-                        break;
-                }
-            }
-            while (cki.Key != ConsoleKey.Escape);  // end do-while
-        } // end try
-        catch (IOException e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        finally
-        {
-            Console.Clear();
-            Console.SetWindowSize(1, 1);
-            Console.SetBufferSize(saveBufferWidth, saveBufferHeight);
-            Console.SetWindowSize(saveWindowWidth, saveWindowHeight);
-            Console.CursorVisible = saveCursorVisible;
-        }
-    } // end Main
-} // end Sample
-*/
-/*
-This example produces results similar to the following:
-
-1) Press the cursor keys to move the console window.
-2) Press any key to begin. When you're finished...
-3) Press the Escape key to quit.
-
-...
-
-+----+----+----+-
-|    |    |    |
-|    |    |    |
-+----+----+----+-
-|    |    |    |
-|    |    |    |
-+----+----+----+-
-
-*/
->>>>>>> feature/MoveAndMap
+        
+        
+      
